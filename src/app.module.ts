66896b6b744a1b1345c9c82c54bf70f6244fe625
Carlos Salazar <carlossalazar@MacBook-Air-de-Carlos.local>
2024-11-26 13:22:16 +0100

    Implementado modulo Common Implementado HashService con Bcrypt en modulo Common Implementadas funciones hashData y compareData en HashService

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest'), UsersModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost/nest'), UsersModule, CommonModule],
   controllers: [AppController],
   providers: [AppService],
 })
